277f70c5520ad20be9120e601cdc4702c03e4f6f
Sergei Butko <s.butko-2026@kpi.ua>
2020-03-26 14:32:18 +0200

    Лабораторная работа №3 и библиотека классов для неё

---
+++ MyLibrary_Lab3/MyLibrary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyLibrary_Lab3
+{
+    public class MyClass
+    {
+        private bool[] MyArray;
+        private bool logicalSum;
+        public int size;
+
+        public MyClass()
+        {
+            MyArray = new bool[] { true, false, false, true, false, true, true, false, false, true };
+            size = MyArray.Length;
+        }
+        public bool this[int index]
+        {
+            get 
+            { 
+                return !MyArray[index]; 
+            }
+            set
+            {
+                if (index > -1 && index < 10)
+                {
+                    MyArray[index] = value;
+                }
+            }
+        }
+        public bool LogicalSum
+        {
+            get
+            {
+                bool result = MyArray[0];
+                for (int i = 1; i < MyArray.Length; i++)
+                {
+                    result |= MyArray[i];
+                }
+                logicalSum = result;
+                return logicalSum;
+            }
+        }
+    }
+}
---
+++ OOP_Lab3_cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MyLibrary_Lab3;
+
+namespace OOP_Lab3_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass Object = new MyClass();
+
+            Console.Write("Ваш массив: { ");
+            for (int i = 0; i < Object.size; i++)
+            {
+                Console.Write(Object[i] + " ");
+            }
+            Console.WriteLine("}");
+            Console.WriteLine("Его Размерность = " + Object.size);
+            Console.WriteLine("==================================================");
+
+            try
+            {
+                int num1 = 5;
+                Console.WriteLine("Вводим число " + num1 + " принадлежащее диапазону массива.");
+                Console.WriteLine("Элемент с индексом " + num1 + " = " + Object[num1]);
+                Console.WriteLine("==================================================");
+
+
+                int num2 = 15;
+                Console.WriteLine("Вводим число " + num2 + " не принадлежащее диапазону массива.");
+                Console.WriteLine("Элемент с индексом " + num2 + " = " + Object[num2]);
+            }
+            catch
+            {
+                Console.WriteLine("Число не принадлежит диапазону массива!");
+                Console.WriteLine("==================================================");
+
+            }
+
+            Console.WriteLine("Результат логического сложения всех элементов массива = " + Object.LogicalSum);
+
+            Console.ReadKey();
+        }
+    }
+}
