1304a74874429ea6060eb23848e075eb1f0d0141
Sergei Butko <s.butko-2026@kpi.ua>
2020-04-13 15:10:50 +0300

    Лабораторная работа №4 на С# и библиотека классов для неё

---
+++ MyLibrary_Lab4/MyClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyLibrary_Lab4
+{
+    public class MyClass
+    {
+        public string str;
+        private int size;
+        public MyClass()
+        {
+            str = null;
+        }
+        public MyClass(string str)
+        {
+            this.str = str;
+            size = str.Length;
+        }
+        public MyClass(MyClass str)
+        {
+            this.str = str.str;
+            size = str.strLength();
+        }
+
+        public int strLength()
+        {
+            return size;
+        }
+        public static MyClass operator +(MyClass S1, MyClass S2)
+        {
+            return new MyClass { str = S1.str + S2.str };
+        }
+        public static MyClass operator -(MyClass str, char Ch)
+        {
+            string result = "";
+            for(int i = 0; i < str.size; i++)
+            {
+                if(str.str[i] != Ch)
+                {
+                    result += str.str[i];
+                }
+            }
+            MyClass result1 = new MyClass(result);
+            return result1;
+        }
+    }
+}
---
+++ OOP_Lab4_cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MyLibrary_Lab4;
+
+namespace OOP_Lab4_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку символов: ");
+            string str = Console.ReadLine();
+
+            MyClass S1 = new MyClass();
+            MyClass S2 = new MyClass(str);
+            MyClass S3 = new MyClass(S2);
+
+            S2 -= '#';
+            Console.WriteLine("Результат вычитания символа '#' с об'екта S2: " + S2.str);
+
+            S1 = S2 + S3;
+            Console.WriteLine("Результат сложения S2 и S3, добавленый в S1: " + S1.str);
+
+            Console.ReadKey();
+        }
+    }
+}
