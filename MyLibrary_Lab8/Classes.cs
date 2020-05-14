e10fe685b4b79a868ca96a9891034e14f4ecbed0
Sergei Butko <s.butko-2026@kpi.ua>
2020-05-14 11:36:20 +0300

    Лабораторная работа №8 на С# и библиотека классов для неё

---
+++ MyLibrary_Lab8/Classes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyLibrary_Lab8
+{
+    public class MyStack
+    {
+        public delegate void MySt(object sender, EventArgs message);
+        public event MySt Notify;
+
+        private int _Size;
+        private char[] _Array;
+        private int _Top;
+        public MyStack(int Size)
+        {
+            this._Size = Size;
+            this._Top = 0;
+            this._Array = new char[this._Size];
+        }
+        public bool IsFull() { return this._Top == this._Size; }
+        public bool IsEmpty() { return this._Top == 0; }
+
+        public void Push(char Element)
+        {
+            if (this.IsFull())
+            {
+                Notify?.Invoke(this, new StackEventArgs() { Message = "Стек переполнен!" });
+                return;
+            }
+            this._Array[this._Top++] = Element;
+        }
+        public char Pop() { return this._Array[--this._Top]; }
+
+        public int FindElement(char ch)
+        {
+            for (int i = 0; i < _Size; i++)
+            {
+                if (_Array[i] == ch)
+                    return i;
+                else
+                    continue;
+            }
+            return -1;
+        }
+    }
+    public class StackEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ OOP_Lab8_cs/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MyLibrary_Lab8;
+
+namespace OOP_Lab8_cs
+{
+    class Program
+    {
+        delegate int IndexOf(char ch);
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер стека: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            MyStack Line = new MyStack(size);
+
+            Console.WriteLine("Заполните стек символами:");
+            for (int i = 0; i < size; i++)
+                Line.Push(Convert.ToChar(Console.ReadLine()));
+
+            bool alive = true;
+            while (alive)
+            {
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("\t1 - Узнать индекс символа.");
+                Console.WriteLine("\t2 - Добавить элемент.");
+                Console.WriteLine("\t3 - Удалить элемент.");
+                Console.WriteLine("\t4 - Выйти из программы.");
+                Console.Write("Ваш выбор: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Введите символ: ");
+                        char ch1 = Convert.ToChar(Console.ReadLine());
+                        IndexOf del = Line.FindElement;
+                        int result = del(ch1);
+                        if (result != -1)
+                            Console.WriteLine($"Индекс символа '{ch1}' = {result}");
+                        else
+                            Console.WriteLine($"Строка не содержит символ '{ch1}'!");
+                        break;
+                    case 2:
+                        Line.Notify += DisplayMessage;
+                        Console.Write("Введите новый символ: ");
+                        Line.Push(Convert.ToChar(Console.ReadLine()));
+                        break;
+                    case 3:
+                        Console.WriteLine($"Удален символ '{Line.Pop()}'");
+                        break;
+                    case 4:
+                        alive = false;
+                        continue;
+                    default:
+                        Console.WriteLine("Действие не найдено!");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+        private static void DisplayMessage(object sender, EventArgs message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
