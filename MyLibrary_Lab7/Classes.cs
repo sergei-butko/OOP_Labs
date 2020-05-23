2b73e19122c012f8e5d10de654a7ed63732b24fe
Sergei Butko <s.butko-2026@kpi.ua>
2020-05-23 22:05:43 +0300

    Лабораторная работа №7 на С# и библиотека классов для неё

---
+++ MyLibrary_Lab7/Classes.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MyLibrary_Lab7
+{
+    public class Node
+    {
+        public Node(short data) { Data = data; }
+        public short Data { get; set; }
+        public Node Next { get; set; }
+}
+    public class MyLinkedList
+    {
+        public Node first;
+        private Node last;
+        private int count;
+
+        public void AddFirst(short data)
+        {
+            Node node = new Node(data);
+            node.Next = first;
+            first = node;
+            if (count == 0)
+                last = first;
+            count++;
+        }
+        public void RemoveUnderAverage(double average)
+        {
+            Node current = first;
+            Node previous = null;
+
+            while (current != null)
+            {
+                if (current.Data < average)
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                            last = previous;
+                    }
+                    else
+                    {
+                        first = first.Next;
+
+                        if (first == null)
+                            last = null;
+                    }
+                    count--;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+        }
+        public int CountMultipleOfThree()
+        {
+            int counter = 0;
+            Node current = first;
+            while (current != null)
+            {
+                if (current.Data % 3 == 0 && current.Data != 0)
+                    counter++;
+                current = current.Next;
+            }
+            return counter;
+        }    
+    }
+}
---
+++ OOP_Lab7_cs/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MyLibrary_Lab7;
+
+namespace OOP_Lab7_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер односвязного списка: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            MyLinkedList MyList = new MyLinkedList();
+
+            Console.WriteLine("Заполните список числами (добавление в начало)");
+            int i = 0;
+            while (i < size)
+            {
+                short input = Convert.ToInt16(Console.ReadLine());
+                if (input >= -32768 && input <= 32767)
+                {
+                    MyList.AddFirst(input);
+                    i++;
+                }
+                else
+                    continue;
+            }
+                
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.Write("Ваш список: [ ");
+            for (Node node = MyList.first; node != null; node = node.Next)
+                Console.Write($"{node.Data} ");
+            Console.Write("]\n");
+
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine($"Список имеет {MyList.CountMultipleOfThree()} элементов кратных 3");
+
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Удаление элементов меньших за среднее значение");
+            int sum = 0;
+            for (Node node = MyList.first; node != null; node = node.Next)
+                sum += node.Data;
+            double average = sum / size;
+            Console.WriteLine($"Среднее значение списка = {average}");
+            MyList.RemoveUnderAverage(average);
+            Console.Write("Ваш список после удаления: [ ");
+            for (Node node = MyList.first; node != null; node = node.Next)
+                Console.Write($"{node.Data} ");
+            Console.Write("]");
+
+            Console.ReadKey();
+        }
+    }
+}
